bcefb9db636af94b1155faad257031317f96f063
shahrizod3021 <shahrizodmirzaaliyevdev@gmail.com>
2023-09-03 19:10:41 +0500

    seventh commit

---
+++ src/main/java/upgrade/upgbot/Controller/ProductsController.java
@@ -0,0 +1,31 @@
+package upgrade.upgbot.Controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import upgrade.upgbot.Entity.Product;
+import upgrade.upgbot.Repository.ProductRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/product")
+public class ProductsController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    public HttpEntity<?> getAll(){
+        List<Product> all = productRepository.findAll();
+        return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/name")
+    public HttpEntity<?> getByName(@RequestParam(name = "name") String name){
+        List<Product> productByName = productRepository.findProductByName(name);
+        return ResponseEntity.ok(productByName);
+    }
+}
